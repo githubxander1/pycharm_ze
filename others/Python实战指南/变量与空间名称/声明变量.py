55aa5238d9e9ac929fac23039a57fe7e3836aae2
2695418206@qq.com <2695418206@qq.com>
2023-09-03 17:09:49 +0800

    1

--- others/Python实战指南/变量与空间名称/声明变量.py
+++ others/Python实战指南/变量与空间名称/声明变量.py
@@ -1,10 +1,38 @@
-v1=True
-v2='start'
-v3=10
-v4=[1,2,3]
+# v1=True
+# v2='start'
+# v3=10
+# v4=[1,2,3]
+#
+# print(f'v1的数据类型是：{type(v1)}')
+# print(f'v2的数据类型是：{v2.__class__.__name__}')
+# v3.hassttr('__name__')
+# print(f'v3的数据类型是：{v3.__class__.__name__}')
+# print(f'v4的数据类型是：{v4.__class__.__name__}')
 
-print(f'v1的数据类型是：{type(v1)}')
-print(f'v2的数据类型是：{v2.__class__.__name__}')
-v3.hassttr('__name__')
-print(f'v3的数据类型是：{v3.__class__.__name__}')
-print(f'v4的数据类型是：{v4.__class__.__name__}')
+# 组合赋值与表达式列表
+v1,v2,v3='a','b','c'
+(v4,v5,v6)=('a','b','c')
+# [v1,v2,v3]=['a','b','c']
+# # [v1,v2,v7]=('a','b','c')
+# a,*b=1,2,3
+# *a,b=1,2,3
+# *a,*b=1,2,3
+# *a=1,2,3
+# (*a)=1,2,3
+
+# [*q]='abcvd'
+# print(f'q:{q}')
+
+# nonlocal关键字
+def test_nunlocal():
+    a='a'
+    def inner1():
+        nonlocal a
+        a='b-'+a
+    def inner2():
+        nonlocal a
+        a='c-'+a
+    inner1()
+    inner2()
+    return a
+print(test_nunlocal())
