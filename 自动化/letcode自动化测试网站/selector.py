ac174bf8d853cd987560f7dd0e2b90b63cf75f31
Admin <2695418206@qq.com>
2023-09-13 19:30:19 +0800

    修改

--- 自动化/letcode自动化测试网站/selector.py
+++ 自动化/letcode自动化测试网站/selector.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 
@@ -6,7 +8,14 @@ d.implicitly_wait(10)
 d.get('https://letcode.in/dropdowns')
 
 # Select the apple using visible text
-select=Select(d.find_element_by_id('fruits'))
-select.select_by_value('Apple')
-select.select_by_index(1)
+# select=Select(d.find_element_by_id('fruits'))
+# # select.select_by_value('Apple')
+# select.select_by_index(1)
+# sleep(2)
+# select.select_by_visible_text('Banana')
+# select.select_by_value('Orange')
 
+sleep(2)
+# 滚动条
+slide = d.find_element_by_css_selector('#superheros > option:nth-child(21)')
+d.execute_script("arguments[0].scrollIntoView();", slide)
