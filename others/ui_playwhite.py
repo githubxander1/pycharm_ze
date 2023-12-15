a8cf3324c12ee55bc599f7d12ce33b9fdb07bbde
Admin <2695418206@qq.com>
2023-12-15 19:13:54 +0800

    1

---
+++ others/ui_playwhite.py
@@ -0,0 +1,24 @@
+import playwright
+# 异步模式
+import asyncio
+from playwright.async_api import async_playwright
+
+async def run(playwright):
+    # 创建一个浏览器实例
+    chromium = playwright.chromium
+    browser = await chromium.launch()
+    # browser = await chromium.launch(channel="msedge")
+    # page=await browser.new_page()
+    # await page.goto("https://www.baidu.com")
+    # await page.screenshot(path="baidu.png")
+    # print(await page.title())
+    # 创建两个浏览器上下文
+    user_context = await browser.new_context()
+    admin_context = await browser.new_context()
+
+    # 创建各种页面和上下文的交互
+
+async def main():
+    async with async_playwright() as playwright:
+        await run(playwright)
+asyncio.run(main())
